50e78cb13e670302aa5367f2b7b3f533b9fc4085
pg <Paul@guoxianlongdeMacBook-Pro.local>
2015-07-21 17:10:48 +0800

    Initial Commit

---
+++ PGScViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PGScViewDemo
+//
+//  Created by guoxianlong on 15/7/21.
+//  Copyright (c) 2015年 guoxianlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PGScViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PGScViewDemo
+//
+//  Created by guoxianlong on 15/7/21.
+//  Copyright (c) 2015年 guoxianlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
